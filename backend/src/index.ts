82c21fb6766ced9f7fdbcdeb93e49c19fbc179ce
prajwal <prajwalkreddy17@gmail.com>
2025-11-11 17:52:00 +0530

    integerate gemini api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,68 @@
 import express from "express";
 import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 const PORT = process.env.PORT;
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 if (!PORT) {
   throw new Error("Missing environment variable: PORT");
 }
 
+if (!GEMINI_API_KEY) {
+  throw new Error("Missing environment variable: GEMINI_API_KEY");
+}
+
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
 app.get("/", (req, res) => {
   console.log("get req");
   res.json({ status: "success" }).status(200);
+});
+
+app.post("/api/chat", async (req, res) => {
+  res.setHeader("Content-type", "text/event-stream");
+  res.setHeader("Cache-control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  try {
+    const { prompt } = req.body;
+
+    const response = await ai.models.generateContentStream({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    for await (const chunk of response) {
+      const text = chunk.text;
+      if (text) {
+        res.write(`data:${JSON.stringify({ text })}\n\n`);
+      }
+
+      // console.log(chunk.text());
+    }
+
+    res.write(`data: [DONE]\n\n`);
+    res.end();
+
+    // res.json({
+    //   status: "success",
+    //   // data: response.text,
+    // });
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
 });
 
 app.listen(PORT, () => {
